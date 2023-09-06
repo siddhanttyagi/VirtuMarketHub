01a5c330a7bd969b6653cad843313b2907265855
siddhanttyagi <siddhanttyagi432@gmail.com>
2023-09-06 14:44:38 +0530

    login-signup-both-working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,16 @@ class SessionsController < ApplicationController
         redirect_to root_path, notice: "Logged out"
     end
     def create
+        @check=true
         user=User.find_by(email: params[:email])
-        if user.present? 
+        if user.present? && user.password == params[:password]
+            session[:user_id]=user.id
+            redirect_to root_path, notice: "Logged in succesfully"
+            @check=true
+        else
+            @check=false
+            render 'users/read', status: :unprocessable_entity
+
+        end
     end
 end
